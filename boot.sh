ac62d6c28ded31447e75a48db436d72a8b8089d1
siyu Jian <jiansiyu@gmail.com>
2019-09-04 23:38:12 -0400

    add the bluetooth configure in the bash

--- boot.sh
+++ boot.sh
@@ -5,7 +5,7 @@ sudo hciconfig hcio up
 # Update  mac address
 ./updateMac.sh
 #Update Name
-./updateName.sh ThanhLe_Keyboard
+./updateName.sh MiaoMiao_Keyboard
 #Get current Path
 export C_PATH=$(pwd)
 #Create Tmux session
@@ -18,6 +18,17 @@ if [ $? != 0 ]; then
     tmux split-window -v -t mlabviet:os.1
     tmux send-keys -t mlabviet:os.0 'cd $C_PATH && sudo /usr/sbin/bluetoothd --nodetach --debug -p time ' C-m
     tmux send-keys -t mlabviet:os.1 'cd $C_PATH/server && sudo python btk_server.py ' C-m
+
     tmux send-keys -t mlabviet:os.2 'cd $C_PATH && sudo /usr/bin/bluetoothctl' C-m
+    sleep 2
+    tmux send-keys -t mlabviet:os.2 'agent on' C-m
+    sleep 2
+    tmux send-keys -t mlabviet:os.2 'default-agent' C-m
+    sleep 2
+    tmux send-keys -t mlabviet:os.2 'scan on' C-m
+    sleep 2
+    tmux send-keys -t mlabviet:os.2 'discovery on' C-m
+
     tmux send-keys -t mlabviet:os.3 'cd $C_PATH/keyboard/ && sleep 5 && sudo python kb_client.py' C-m
 fi
+tmux a
